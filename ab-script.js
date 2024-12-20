60c557f952355c6e529faec4ef3b0ed68c48fed2
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-20 10:49:26 -0800

    Update ab-script.js

--- ab-script.js
+++ ab-script.js
@@ -14,9 +14,11 @@ async function fetchVideos() {
     let apiUrl;
 
     
-    const youtubeUrlPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
-    if (youtubeUrlPattern.test(query)) {
-      apiUrl = `https://weeb-api.vercel.app/ytsearch?query=${encodeURIComponent(query)}`;
+    const youtubeUrlPattern = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = query.match(youtubeUrlPattern);
+    if (match && match[1]) {
+      const videoId = match[1];
+      apiUrl = `https://ironman.koyeb.app/search/yts?query=${encodeURIComponent(videoId)}`;
     } else {
       apiUrl = `https://weeb-api.vercel.app/ytsearch?query=${encodeURIComponent(query)}`;
     }

3f73c259ce1a575cd53af23511efbc0466f71448
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-27 08:29:30 -0800

    Update ab-script.js

--- ab-script.js
+++ ab-script.js
@@ -32,17 +32,7 @@ async function fetchVideos() {
   loadingDiv.classList.remove("hidden");
 
   try {
-    let apiUrl;
-
-    const youtubeUrlPattern = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-    const match = query.match(youtubeUrlPattern);
-    if (match && match[1]) {
-      const videoId = match[1];
-      apiUrl = `https://ironman.koyeb.app/search/yts?query=${encodeURIComponent(videoId)}`;
-    } else {
-      apiUrl = `https://weeb-api.vercel.app/ytsearch?query=${encodeURIComponent(query)}`;
-    }
-
+    const apiUrl = `https://weeb-api.vercel.app/ytsearch?query=${encodeURIComponent(query)}`;
     const response = await fetchWithRetry(proxyUrl + encodeURIComponent(apiUrl));
 
     if (response.status === 404) {
